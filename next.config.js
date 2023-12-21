c29e3817d19a374942ac803fb9e3295d5d87db85
ren.siwei3 <ren.siwei3@21vianet.com>
2023-12-21 13:26:38 +0800

    # add dir

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withMDX = require('@next/mdx')();
 
 module.exports = withMDX(withLess({
   output: process.env.EXPORT === 'true' ? 'export' : undefined,
-  // distDir: process.env.EXPORT === 'true' ? 'out' : undefined,
+  distDir: process.env.EXPORT === 'true' ? 'out/careless-whisper' : undefined,
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   lessLoaderOptions: {
