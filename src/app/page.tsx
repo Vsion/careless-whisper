c41e846555a6e5adc29892f8637e6313531b5153
ren.siwei3 <ren.siwei3@21vianet.com>
2023-12-22 14:03:15 +0800

    # multirepo vs monorepo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,6 +30,15 @@ export default function Index() {
           </h2>
           <p>冥想和正念练习是两种强大的心理健康工具，它们有助于提高专注力、减少压力、增强情绪稳定性，并提高总体的生活质量。</p>
         </Link>
+        <Link
+          href="/md/multirepo.vs.monorepo"
+          className={styles.card}
+        >
+          <h2>
+            Multirepo vs Monorepo <span>-&gt;</span>
+          </h2>
+          <p>项目代码管理方式对比: 多仓库多模块应用 vs 单仓库多模块应用</p>
+        </Link>
       </div>
     </>
   )
