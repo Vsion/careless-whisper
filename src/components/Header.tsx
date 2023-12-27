ab21ba1557676e999f454484e02fee603bb76df0
ren.siwei3 <ren.siwei3@21vianet.com>
2023-12-27 17:33:07 +0800

    # use next Image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 import styles from './header.module.css';
 import { basePath } from '../../public/constants';
@@ -6,7 +7,7 @@ import { basePath } from '../../public/constants';
 export default function Header() {
   return <header className={styles.header}>
     <nav className={styles.nav}>
-      <Link href={'/'} replace><img className={styles.logo} src={basePath + '/logo.png'} /></Link>
+      <Link href={'/'} replace><Image height={64} width={64} alt={'vsion'} className={styles.logo} src={basePath + '/logo.png'} /></Link>
       {/* <Link href={'/'} replace><img className={styles.logo} src={'/logo.png'} /></Link> */}
     </nav>
   </header>
