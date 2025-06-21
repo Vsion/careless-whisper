32644af6374edcdf844c0ecef3d6e56a959f1d13
Vsion <442153598@qq.com>
2025-06-22 00:38:34 +0800

    # change aoyin title

--- src/app/md/jp_aoyin/layout.tsx
+++ src/app/md/jp_aoyin/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next';
 import React from 'react';
 
 export const metadata: Metadata = {
-  title: '浊音和半浊音',
+  title: '日语拗音表（平假名+片假名+罗马音）',
 }
 
 export default function PageLayout({ children }: { children: React.ReactNode }) {
